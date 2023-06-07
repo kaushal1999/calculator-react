1b5de7f17843cc307c638fba654da7d91f1eb5fd
kaushal1999 <kaushalwaghela1999@gmail.com>
2023-06-07 18:07:05 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class Display extends React.Component{
+  
+}
+
+class Button extends Reac.Component {
+
+}
+class ButtonPanel extends React.Component{
+   
+  render() {
+    return (
+      <div>
+        <div>
+          <Button name="AC" clickHandler={this.handleClick} />
+          <Button name="+/-" clickHandler={this.handleClick} />
+          <Button name="%" clickHandler={this.handleClick} />
+          <Button name="÷" clickHandler={this.handleClick} orange />
+        </div>
+        <div>
+          <Button name="7" clickHandler={this.handleClick} />
+          <Button name="8" clickHandler={this.handleClick} />
+          <Button name="9" clickHandler={this.handleClick} />
+          <Button name="x" clickHandler={this.handleClick} orange />
+        </div>
+        <div>
+          <Button name="4" clickHandler={this.handleClick} />
+          <Button name="5" clickHandler={this.handleClick} />
+          <Button name="6" clickHandler={this.handleClick} />
+          <Button name="-" clickHandler={this.handleClick} orange />
+        </div>
+        <div>
+          <Button name="1" clickHandler={this.handleClick} />
+          <Button name="2" clickHandler={this.handleClick} />
+          <Button name="3" clickHandler={this.handleClick} />
+          <Button name="+" clickHandler={this.handleClick} orange />
+        </div>
+        <div>
+          <Button name="0" clickHandler={this.handleClick} wide />
+          <Button name="." clickHandler={this.handleClick} />
+          <Button name="=" clickHandler={this.handleClick} orange />
+        </div>
+      </div>
+    );
+  }
+}
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Display></Display>
+        <ButtonPanel></ButtonPanel>
+      </div>
+    );
+  }
+}
+
+export default App;
